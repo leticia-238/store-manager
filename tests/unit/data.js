e94cdd4f667c188f534bf72a79f0ed957b99d3ab
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-01 20:34:08 +0000

    test: altera mensagens dos testes

---
+++ tests/unit/data.js
@@ -0,0 +1,14 @@
+const PRODUCTS_LIST = [
+  { id: 1, name: 'Martelo de Thor' },
+  { id: 2, name: 'Traje de encolhimento' },
+];
+
+const PRODUCT = {
+  id: 1,
+  name: 'Martelo de Thor',
+};
+
+module.exports = {
+  PRODUCTS_LIST,
+  PRODUCT,
+};
--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -3,34 +3,30 @@ const sinon = require('sinon');
 const productsModel = require('../../../models/productsModel');
 const db = require('../../../models/db');
 
-const productsList = [
-  { id: 1, name: 'Martelo de Thor'},
-  { id: 2, name: 'Traje de encolhimento'}
-];
-
-const product = {
-  id: 1,
-  name: 'Martelo de Thor',
-};
+const { PRODUCTS_LIST, PRODUCT } = require('../data');
   
-describe('', function () {
+describe('Teste unitário do productsModel', () => {
   afterEach(sinon.restore);
   
-  it('retorna uma lista de produtos', async function () {
-    sinon.stub(db, 'query').resolves([productsList]);
-    const result = await productsModel.getAll();
-    expect(result).to.be.deep.equal(productsList);
+  describe('lista todos os produtos', () => {
+    it('deve retornar uma lista de produtos', async () => {
+      sinon.stub(db, 'query').resolves([PRODUCTS_LIST]);
+      const result = await productsModel.getAll();
+      expect(result).to.be.deep.equal(PRODUCTS_LIST);
+    });
   });
+  
+  describe('retorna o produto procurado pelo id', () => {
+    it('deve retornar o produto se ele for encontrado', async () => {
+      sinon.stub(db, 'query').resolves([[PRODUCT]]);
+      const result = await productsModel.getById(PRODUCT.id);
+      expect(result).to.be.deep.equal(PRODUCT);
+    });
     
-  it('retorna o produto se ele for encontrado', async function () {
-    sinon.stub(db, 'query').resolves([[product]]);
-    const result = await productsModel.getById(product.id);
-    expect(result).to.be.deep.equal(product);
-  });
-    
-  it('retorna um array vazio se o produto não for encontrado', async function () {
-    sinon.stub(db, 'query').resolves([[]]);
-    const result = await productsModel.getById(product.id);
-    expect(result).to.be.undefined;
+    it('deve retornar um array vazio se o produto não for encontrado', async () => {
+      sinon.stub(db, 'query').resolves([[]]);
+      const result = await productsModel.getById(PRODUCT.id);
+      expect(result).to.be.undefined;
+    });
   });
 });
