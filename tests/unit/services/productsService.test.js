e206b5ff3975b398993e07d37648d5b445f12551
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-01 20:41:22 +0000

    test: adiciona teste unitario do productsService

---
+++ tests/unit/services/productsService.test.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+const NotFoundError = require('../../../errors/NotFoundError');
+const productsModel = require('../../../models/productsModel');
+const productsService = require('../../../services/productsService');
+
+const { PRODUCTS_LIST, PRODUCT } = require('../data');
+
+chai.use(chaiAsPromised);
+
+const { expect } = chai;
+
+const NON_EXISTENT_ID = 91;
+
+describe('Teste unitário do productsService', () => {
+  afterEach(sinon.restore);
+  
+  describe('lista todos os produtos', () => {
+    it('deve retornar uma lista com todos os produtos', async () => {
+      sinon.stub(productsModel, 'getAll').resolves(PRODUCTS_LIST);
+      const result = await productsService.getAll();
+      expect(result).to.be.deep.equal(PRODUCTS_LIST);
+    });
+  });
+  
+  describe('retorna o produto procurado pelo id', () => {
+    it('deve retornar o produto se ele for encontrado', async () => {
+      sinon.stub(productsModel, 'getById').resolves(PRODUCT);
+      const result = await productsService.getById(PRODUCT.id);
+      expect(result).to.be.deep.equal(PRODUCT);
+    });
+    
+    it('dispara um erro se o produto não for encontrado', async () => {
+      sinon.stub(productsModel, 'getById').resolves(undefined);
+      
+      return expect(productsService.getById(NON_EXISTENT_ID))
+        .to.eventually.be.rejectedWith(NotFoundError);
+    });
+  });
+});
