5dfd07a324f2602b5236a9d9542a907c1176aca3
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-05 01:09:00 +0000

    fix: corrige nome do erro

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -7,7 +7,7 @@ const errorHandler = (err, _req, res, _next) => {
     case 'NotFoundError':
       res.status(404).json({ message });
       break;
-    case 'UnprocessableEntity':
+    case 'UnprocessableEntityError':
       res.status(422).json({ message });
       break;
     default: res.status(500).send({ message });
